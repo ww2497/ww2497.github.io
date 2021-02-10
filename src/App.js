72b36a3dea7cabd135525ce2a8159dee6f8bebd4
ww2497 <ww2497@rit.edu>
2021-02-10 01:23:34 -0500

    Basic nav functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
+import * as Scroll from 'react-scroll';
+import { Link, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
+import { Waypoint } from 'react-waypoint';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons'
 import { faTools } from '@fortawesome/free-solid-svg-icons'
@@ -9,24 +13,45 @@ import rainImage from './assets/rain.jpg';
 import rainVideo from './assets/rain.mp4';
 
 class App extends React.Component {
+	componentDidMount() {
+		var vid = document.getElementById("rain-video");
+		vid.play(); // get around autoplay being disabled by low power mode
+	}
+
+	showNav(data) {
+		document.getElementById("nav").classList.add("show");
+	}
+	hideNav(data) {
+		document.getElementById("nav").classList.remove("show");
+	}
+	opaqueNav(data) {
+		document.getElementById("nav").classList.add("opaque");
+	}
+	transparentNav(data) {
+		document.getElementById("nav").classList.remove("opaque");
+	}
+
 	render() {
+		const scrollDuration = 500;
 		return (
 			<div>
-				<nav>
+				<nav id="nav">
 					<ul>
-						<a href="#home"><li>Home</li></a>
-						<a href="#about"><li>About</li></a>
-						<a href="#experience"><li>Experience</li></a>
-						<a href="#projects"><li>Projects</li></a>
-						<a href="#contact"><li>Contact</li></a>
+						<li id="navHome"><Link activeClass="active" to="home" spy={true} smooth={true} duration={scrollDuration} onSetActive={this.transparentNav}>Home</Link></li>
+						<li id="navAbout"><Link activeClass="active" to="about" spy={true} smooth={true} duration={scrollDuration} onSetActive={this.opaqueNav}>About</Link></li>
+						<li id="navResume"><Link activeClass="active" to="resume" spy={true} smooth={true} duration={scrollDuration} onSetActive={this.opaqueNav}>Résumé</Link></li>
 					</ul>
 				</nav>
 				<section id="home">
 					<img id="rain-image" src={rainImage} alt="Graphic of a rainy day in the city through a window." />
-					<video id="rain-video" title="Animated graphic of a rainy day in the city through a window." loop autoPlay muted playsInline>
+					<video id="rain-video" title="Animated graphic of a rainy day in the city through a window." loop muted playsInline>
 						<source src={rainVideo} type="video/mp4" />
 						Your browser does not support the video tag.
 					</video>
+
+					<Waypoint onEnter={this.showNav} onLeave={this.hideNav}>
+						<div></div>
+					</Waypoint>
 
 					<div id="home-content">
 						<h1 id="name">
@@ -44,11 +69,28 @@ class App extends React.Component {
 							<span>Under construction!</span>
 						</div>
 					</div>
+
+					<div></div>
 				</section>
 				<section id="about">
+					<br/>
+					<br/>
+					<br/>
+					<br/>
+					<br/>
 					About
 					<br/>
 					Hello! My name is William.
+					<br/>
+					This part is also under construction.
+				</section>
+				<section id="resume">
+					<br/>
+					<br/>
+					<br/>
+					<br/>
+					<br/>
+					Bees?
 				</section>
 			</div>
 		);
