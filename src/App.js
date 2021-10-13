8a172fd09098ce9aea523d5f782c60a617dfd61d
ww2497 <ww2497@rit.edu>
2021-10-13 18:44:52 -0400

    Update resume, use sticky

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
-import * as Scroll from 'react-scroll';
-import { Link, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
+import { Link } from 'react-scroll'
 import { Waypoint } from 'react-waypoint';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -13,48 +12,30 @@ import rainImage from './assets/rain.jpg';
 import rainVideo from './assets/rain.mp4';
 
 class App extends React.Component {
-	constructor(props) {
-		super(props);
-	}
-
-	componentDidMount() {
-	}
 	showNav(data) {
-		document.getElementById("nav").classList.add("transparent");
+		document.getElementById("nav").classList.remove("invisible");
 	}
 	hideNav(data) {
-		document.getElementById("nav").classList.remove("transparent");
-	}
-	transparentNav(data) {
-		document.getElementById("nav").classList.remove("opaque");
-		document.getElementById("nav-background").classList.remove("fixed");
-	}
-	opaqueNav(data) {
-		document.getElementById("nav").classList.add("opaque");
-		document.getElementById("nav-background").classList.add("fixed");
+		document.getElementById("nav").classList.add("invisible");
 	}
 	playVideo(data) {
-		var vid = document.getElementById("rain-video");
-		vid.play();
+		document.getElementById("rain-video").play();
 	}
-	stopVideo(data) {
-		var vid = document.getElementById("rain-video");
-		vid.pause();
+	pauseVideo(data) {
+		document.getElementById("rain-video").pause();
 	}
-
 	render() {
 		const scrollDuration = 500;
 		return (
 			<div>
 				<nav id="nav">
 					<ul>
-						<li id="navHome"><Link activeClass="active" to="home" spy={true} smooth={true} duration={scrollDuration} onSetActive={this.transparentNav}>Home</Link></li>
-						<li id="navAbout"><Link activeClass="active" to="about" spy={true} smooth={true} duration={scrollDuration} onSetActive={this.opaqueNav}>About</Link></li>
-						<li id="navResume"><Link activeClass="active" to="resume" spy={true} smooth={true} duration={scrollDuration} onSetActive={this.opaqueNav}>Résumé</Link></li>
+						<li id="navHome"><Link activeClass="active" to="home" spy={true} smooth={true} duration={scrollDuration} onSetActive={this.hideNav}>Home</Link></li>
+						<li id="navAbout"><Link activeClass="active" to="about" spy={true} smooth={true} duration={scrollDuration} onSetActive={this.showNav}>About</Link></li>
+						<li id="navResume"><Link activeClass="active" to="resume" spy={true} smooth={true} duration={scrollDuration}>Résumé</Link></li>
 					</ul>
 				</nav>
-				<Waypoint onEnter={this.playVideo} onLeave={this.stopVideo}>
-				</Waypoint>
+				<Waypoint onEnter={this.playVideo} onLeave={this.pauseVideo}></Waypoint>
 				<section id="home">
 					<img id="rain-image" src={rainImage} alt="Graphic of a rainy day in the city through a window." />
 					<video id="rain-video" title="Animated graphic of a rainy day in the city through a window." loop muted playsInline>
@@ -66,18 +47,18 @@ class App extends React.Component {
 						<div></div>
 					</Waypoint>
 
-					<div class="content">
+					<div className="content">
 						<h1 id="name">
 							William Wu
 						</h1>
 						<h2 id="roles">
 							Web Developer | Game Developer
 						</h2>
-						<div id="socials">
-							<a id="icon-github" href="https://github.com/ww2497" target="_blank">
+						<div id="urls">
+							<a id="icon-github" href="https://github.com/ww2497" target="_blank" rel="noreferrer">
 								<FontAwesomeIcon icon={faGithub} />
 							</a>
-							<a id="icon-linkedin" href="https://www.linkedin.com/in/ww2497/" target="_blank">
+							<a id="icon-linkedin" href="https://www.linkedin.com/in/ww2497/" target="_blank" rel="noreferrer">
 								<FontAwesomeIcon icon={faLinkedin} />
 							</a>
 							<a id="icon-mail" href="mailto:ww2497@rit.edu">
@@ -89,18 +70,19 @@ class App extends React.Component {
 							<span>Under construction!</span>
 						</div>
 					</div>
+
 					<div></div>
 				</section>
 				<section id="about">
 					<div id="nav-background"></div>
-					<div class="content">
+					<div className="content">
 						<div id="about-introduction">
-							<h1 class="section-name">
+							<h1 className="section-name">
 								About
 							</h1>
-							<h1 class="white">
+							<h2>
 								Hey there!
-							</h1>
+							</h2>
 							<p>
 								I'm <span>William</span>, an undergraduate <span>Computer Science</span> student at the <span>Rochester Institute of Technology</span>.
 								My expected graduation date is <span>May 2023</span>.
@@ -109,7 +91,7 @@ class App extends React.Component {
 						</div>
 						<div id="about-info">
 							<div>
-								<h3 class="white">OBJECTIVE</h3>
+								<h3>OBJECTIVE</h3>
 								<p>
 									Currently, I'm open to internships taking place during any time in 2022.
 								</p>
@@ -118,7 +100,7 @@ class App extends React.Component {
 								</p>
 							</div>
 							<div>
-								<h3 class="white">INFORMATION</h3>
+								<h3>INFORMATION</h3>
 								<p>
 									Under construction!
 								</p>
@@ -127,8 +109,8 @@ class App extends React.Component {
 					</div>
 				</section>
 				<section id="resume">
-					<div class="content">
-						<h1 class="section-name">
+					<div className="content">
+						<h1 className="section-name">
 							Résumé
 						</h1>
 						<p>
